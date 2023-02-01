a652c11add8cbc76f09e5ee773201a9afe4de929
Jeyabala <83962927+Jeyabala@users.noreply.github.com>
2023-02-01 11:28:10 +0530

    Add project files.

---
+++ src/apis/APIModel/APIModel/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIModel.Controllers
+{
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("api/test")]
+        public async Task<ActionResult<string>> TestApi()
+        {
+            return "Hello world";
+        }
+    }
+}
